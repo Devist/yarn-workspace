60b66805643f337f232e3ff3c9472162e7438110
오동환 / 서비스개발담당 WEB개발팀 <215292@ssg.com>
2023-03-03 12:25:13 +0900

    feat: tutorial 구현

--- packages/web/todo-svelte/src/App.svelte
+++ packages/web/todo-svelte/src/App.svelte
@@ -1,5 +1,22 @@
+<script>
+  export let name;
+
+  function toggleName() {
+    if (name === "world") {
+      name = "svelte";
+    } else {
+      name = "world";
+    }
+  }
+</script>
+
 <main>
-  <h1 class="text-3xl font-bold underline bg-error text-white">hi</h1>
+  <h1>안녕! {name}!</h1>
+  <button on:click={toggleName}>토글 name</button>
+  <p>
+    <a href="https://svelte.dev/tutorial">Svelte 튜토리얼</a>에 방문해서 Svelte
+    앱을 반드는 방법을 배워보세요.
+  </p>
 </main>
 
 <style global lang="postcss">

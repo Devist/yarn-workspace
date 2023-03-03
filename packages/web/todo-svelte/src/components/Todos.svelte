2e0586c36c5aae8d0f4bbca8c4401dffb04e959c
Devist <aboutdh89@naver.com>
2023-03-03 15:28:35 +0900

    feat: 컴포넌트 간 데이터 공유: 방법 1 - passing a handler as a prop

--- packages/web/todo-svelte/src/components/Todos.svelte
+++ packages/web/todo-svelte/src/components/Todos.svelte
@@ -1,4 +1,6 @@
 <script>
+  import FilterButton from "./FilterButton.svelte";
+
   export let todos = [];
 
   $: totalTodos = todos.length;
@@ -48,38 +50,7 @@
   </form>
 
   <!-- Filter -->
-  <div class="filters btn-group stack-exception">
-    <button
-      class="btn toggle-btn"
-      class:btn__primary={filter === "all"}
-      aria-pressed={filter === "all"}
-      on:click={() => (filter = "all")}
-    >
-      <span class="visually-hidden">Show</span>
-      <span>All</span>
-      <span class="visually-hidden">tasks</span>
-    </button>
-    <button
-      class="btn toggle-btn"
-      class:btn__primary={filter === "active"}
-      aria-pressed={filter === "active"}
-      on:click={() => (filter = "active")}
-    >
-      <span class="visually-hidden">Show</span>
-      <span>Active</span>
-      <span class="visually-hidden">tasks</span>
-    </button>
-    <button
-      class="btn toggle-btn"
-      class:btn__primary={filter === "completed"}
-      aria-pressed={filter === "completed"}
-      on:click={() => (filter = "completed")}
-    >
-      <span class="visually-hidden">Show</span>
-      <span>Completed</span>
-      <span class="visually-hidden">tasks</span>
-    </button>
-  </div>
+  <FilterButton {filter} onclick={(clicked) => (filter = clicked)} />
 
   <!-- TodosStatus -->
   <h2 id="list-heading">{completedTodos} / {totalTodos} 완료</h2>
